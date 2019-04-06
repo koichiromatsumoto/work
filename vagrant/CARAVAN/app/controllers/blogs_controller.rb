16da66aa4d7a5ec6036cfdfa9287f0c28461e1bd
松本航一郎 <matsumotokouichirou@matsumotokouichirounoMacBook-Pro.local>
2019-04-06 20:56:52 +0900

    [finish]4-4

--- vagrant/CARAVAN/app/controllers/blogs_controller.rb
+++ vagrant/CARAVAN/app/controllers/blogs_controller.rb
@@ -3,6 +3,7 @@ class BlogsController < ApplicationController
 	end
 
 	def index
+		@blogs = Blog.all
 	end
 
 	def new
@@ -10,7 +11,7 @@ class BlogsController < ApplicationController
 	end
 
 	def create
-		@blog = Blog.new(post_params)
+		@blog = Blog.new(blog_params)
         @blog.save
         redirect_to blogs_path
 	end
