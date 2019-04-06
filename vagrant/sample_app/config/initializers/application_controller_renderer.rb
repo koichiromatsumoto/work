955d690c5d47076fa1321a4db6545ae00779b4ed
松本航一郎 <matsumotokouichirou@matsumotokouichirounoMacBook-Pro.local>
2019-04-06 19:41:00 +0900

    [finish]3-10

---
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '54963128ea88e67abcdedc1c0352f58ffa2211105bae058b621b5930abcf63d38cadef3ad96e38cc6370a0ceda43a6c670bc96ae9482cbdf7246e8dde989061e'
