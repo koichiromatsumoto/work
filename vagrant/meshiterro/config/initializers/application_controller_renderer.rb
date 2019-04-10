8fae9b082cad1e2d19ff76eebaae03e9b8dd02ae
松本航一郎 <matsumotokouichirou@matsumotokouichirounoMacBook-Pro.local>
2019-04-10 21:43:33 +0900

    [finish]5-6 次7章から

---
+++ vagrant/meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'ae9701dc763344bc4b18e464892ab21efad10620d9c7fd46dd32dfcad5662c9968789fc88b652fd02858d144e95b63221b9051389576abc6c4921e5bb413b0b9'
