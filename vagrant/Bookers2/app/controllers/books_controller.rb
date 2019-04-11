f045c26b6f7b6d98a5222855047004be70d7cb61
松本航一郎 <matsumotokouichirou@matsumotokouichirounoMacBook-Pro.local>
2019-04-11 21:55:04 +0900

    [start]5-9トップページのpath途中

---
+++ vagrant/Bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		@book.update
+		redirect_to book_path(@book.id)
+	end
+
+	def index
+		@books = Book.all
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def destroy
+		@book = Book.find(params[:id])
+		@book.destroy
+		redirect_to books_path
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:book_title, :book_body)
+	end
+
+end
